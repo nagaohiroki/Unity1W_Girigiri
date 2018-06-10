7b6aad57d73e69f18b317f1c8523a48ad0904b2a
nagaohiroki <testplayers0226@gmail.com>
2018-06-10 11:42:01 +0900

    ゲーム追加

---
+++ UnityProject/Assets/Dog.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+public class Dog : MonoBehaviour
+{
+	[SerializeField]
+	Rigidbody mRigidbody;
+	[SerializeField]
+	GameObject mCameraHandle;
+	[SerializeField]
+	GameObject mHead;
+	[SerializeField]
+	GameObject mTail;
+	void Update()
+	{
+		const float power = 10.0f;
+		if(Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			AddForce(mHead, power);
+			AddForce(mTail, -power);
+		}
+		if(Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			AddForce(mHead, -power);
+			AddForce(mTail, power);
+		}
+		mCameraHandle.transform.position = transform.position;
+	}
+	void AddForce(GameObject inGameObject, float inPower)
+	{
+		if(inGameObject == null || mRigidbody == null)
+		{
+			Debug.LogError("Dogのinspectorをよく見て");
+			return;
+		}
+		var trans = inGameObject.transform;
+		mRigidbody.AddForceAtPosition(trans.forward * inPower, trans.position, ForceMode.VelocityChange);
+	}
+}
