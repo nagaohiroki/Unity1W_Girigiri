79160a248150dd61f1f901125443a81cb6cf2b21
nagaohiroki <testplayers0226@gmail.com>
2019-10-29 21:15:35 +0900

    不要なコード削除

--- UnityProject/Assets/Dog.cs
+++ UnityProject/Assets/Dog.cs
@@ -5,21 +5,12 @@ using UnityEngine.UI;
 // ------------------------------------------------------------------------
 public class Dog : MonoBehaviour
 {
-	enum Arrow
-	{
-		None,
-		Left,
-		Right,
-	}
 	// タイマー
 	[SerializeField]
 	Timer mTimer = null;
 	// 剛体キャッシュ
 	[SerializeField]
 	Rigidbody mRigidbody = null;
-	// カメラのハンドル
-	[SerializeField]
-	GameObject mCameraHandle = null;
 	// クリアのメッセージ
 	[SerializeField]
 	Text mClearMessage = null;
@@ -74,59 +65,6 @@ public class Dog : MonoBehaviour
 		}
 	}
 	// ------------------------------------------------------------------------
-	/// @brief 操作
-	///
-	/// @param inPower
-	// ------------------------------------------------------------------------
-	void Move(float inPower)
-	{
-		Arrow arrow = GetArrow();
-		// 左回転
-		if(arrow == Arrow.Left)
-		{
-			AddTorque(inPower);
-		}
-		// 右回転
-		if(arrow == Arrow.Right)
-		{
-			AddTorque(-inPower);
-		}
-		// 再スタート
-		if(Input.GetKeyDown(KeyCode.S))
-		{
-			Restart();
-		}
-	}
-	Arrow GetArrow()
-	{
-		if(Input.GetButtonDown("Fire1"))
-		{
-			if(0.0f > Input.mousePosition.x - Screen.width / 2.0f)
-			{
-				return Arrow.Left;
-			}
-			return Arrow.Right;
-		}
-		if(Input.GetKeyDown(KeyCode.RightArrow))
-		{
-			return Arrow.Right;
-		}
-		if(Input.GetKeyDown(KeyCode.LeftArrow))
-		{
-			return Arrow.Left;
-		}
-		return Arrow.None;
-	}
-	// ------------------------------------------------------------------------
-	/// @brief 回転
-	///
-	/// @param inPower
-	// ------------------------------------------------------------------------
-	void AddTorque(float inPower)
-	{
-		mRigidbody.AddTorque(Vector3.forward * inPower, ForceMode.VelocityChange);
-	}
-	// ------------------------------------------------------------------------
 	/// @brief 初回更新
 	// ------------------------------------------------------------------------
 	void Start()
@@ -138,9 +76,14 @@ public class Dog : MonoBehaviour
 	// ------------------------------------------------------------------------
 	void Update()
 	{
+		// リセット
+		if(Input.GetKeyDown(KeyCode.S))
+		{
+			Restart();
+		}
 		// 移動
-		Move(10.0f);
+		mRigidbody.AddTorque(Vector3.back * Input.GetAxis("Horizontal"), ForceMode.VelocityChange);
 		// カメラ移動
-		mCameraHandle.transform.position = transform.position;
+		Camera.main.transform.parent.position = transform.position;
 	}
 }
